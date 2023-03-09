b0a6accd2e3d53d5d2403509aeb7be86a91595bc
Niall Horgan <G00426047@atu.ie>
2023-03-09 17:40:01 +0000

    Week 02 Task

--- bank.py
+++ bank.py
@@ -1,3 +1,5 @@
+# bank.py
+
 #Read 2 numbers(integers), add both and return answer in EURO format
 
 #Author: Niall Horgan
@@ -8,3 +10,5 @@ num2 = int(input('Enter amount2 (in cents): '))
 sum = num1 + num2
 
 print (f'The sum of these 2 numbers in EUROs is: €{sum/100}')
+print (f'The sum of these 2 numbers in EUROs is: €{sum/100}')
+print ('The sum of these 2 numbers in EUROs is: €{}'.format(sum/100))
