31ba3889817f2ada9576df774bdfb1db2f9fbab6
Niall Horgan <G00426047@atu.ie>
2023-03-29 18:37:12 +0100

    Week 08 Task

--- plottask.py
+++ plottask.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statistics
 
+m = 10
+sd = 2
+
 np.random.seed(1)
-norm_data = np.random.normal(size=1000, )
+norm_data = np.random.normal(m, sd, size=1000 )
 
 plt.hist(norm_data, edgecolor = 'k')
 
-m = statistics.mean(norm_data)
-sd = statistics.stdev(norm_data)
-
 plt.axvline(m, color ='k', linestyle = 'dashed')
-
-plt.axvline(sd, color ='k', linestyle = 'dashed')
-plt.axvline(sd, color ='k', linestyle = 'dashed')
+plt.title('Normal Distribution (Mean = 10, STD = 2)', fontsize = 15)
+plt.xlabel('Values of random Varible X', fontsize = 10)
+plt.ylabel('Probability', fontsize = 10)
 
 plt.show()
